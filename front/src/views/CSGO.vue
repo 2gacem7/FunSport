8996543fd867922658a926adfa339aebd980517b
Yobar73 <bthl.yohann73@gmail.com>
2020-10-08 11:06:42 +0200

    move env.config to the front and import module in CSGO and LOL

--- front/src/views/CSGO.vue
+++ front/src/views/CSGO.vue
@@ -1,68 +1,66 @@
 <template>
   <div class="home">
     <Navbar />
-    <TabBar/>
+    <TabBar />
     <AddMySport />
     CSGO
-    <b-card class=" col-md-4"><CalendarCsgo /></b-card>
+    <b-card class="col-md-4"><CalendarCsgo /></b-card>
   </div>
 </template>
 
 <script>
-  import Navbar from "@/components/NavBar.vue";
-      import TabBar from "@/components/TabBar.vue";
-        import CalendarCsgo from "@/components/CalendarCsgo.vue";
+import Navbar from "@/components/NavBar.vue";
+import TabBar from "@/components/TabBar.vue";
+import CalendarCsgo from "@/components/CalendarCsgo.vue";
 
-      import AddMySport from "@/components/AddMySport.vue";
+import AddMySport from "@/components/AddMySport.vue";
+import ENV from "../../env.config";
 
-  export default {
-    name: "CSGO",
-    components: {
-      Navbar,
-      TabBar,
-      AddMySport,
-      CalendarCsgo
-    },
-    data() {
-      return {
-      };
-    },
-    mounted() {
-
-    },
-    methods: {
-
-      getCookie() {
-        let access_token = "";
-        if (document.cookie.length > 0) {
-          let cookieArray = document.cookie.split(";");
-          for (let i = 0; i < cookieArray.length; i++) {
-            if (cookieArray[i].indexOf("My_FunSport_Token") != -1) {
-              const cookiename = "My_FunSport_Token=";
-              access_token = cookieArray[i].substring(
-                cookiename.length,
-                cookieArray[i].length
-              );
-            }
+export default {
+  name: "CSGO",
+  components: {
+    Navbar,
+    TabBar,
+    AddMySport,
+    CalendarCsgo,
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+  },
+  methods: {
+    getCookie() {
+      let access_token = "";
+      if (document.cookie.length > 0) {
+        let cookieArray = document.cookie.split(";");
+        for (let i = 0; i < cookieArray.length; i++) {
+          if (cookieArray[i].indexOf("My_FunSport_Token") != -1) {
+            const cookiename = "My_FunSport_Token=";
+            access_token = cookieArray[i].substring(
+              cookiename.length,
+              cookieArray[i].length
+            );
           }
         }
-        return access_token;
-      },
+      }
+      return access_token;
     },
-  };
+  },
+};
 </script>
 <style>
-  .content {
-    padding-top: 70px;
-    padding-left: 220px;
-  }
+.content {
+  padding-top: 70px;
+  padding-left: 220px;
+}
 
-  body {
-    background-color: rgb(29, 28, 28);
-    color: white;
-  }
+body {
+  background-color: rgb(29, 28, 28);
+  color: white;
+}
 
-  .update {
-    font-size: 0.75rem !important;
-  }
+.update {
+  font-size: 0.75rem !important;
+}
 </style>
