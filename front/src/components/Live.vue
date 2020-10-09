9e4c44631498af5d819824bd25a6d044d3909f51
Yobar73 <bthl.yohann73@gmail.com>
2020-10-09 16:45:26 +0200

    add icon trash in dashboard

--- front/src/components/Live.vue
+++ front/src/components/Live.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card m-3 bg-light" style="max-height: 30rem; max-width: 20rem">
-    <div class="card-header justify-content-between">
+    <div class="card-header  d-flex justify-content-between">
       <button
         v-if="!delButton"
         class="btn btn-success font-weight-bold mb-2"
@@ -14,7 +14,10 @@
         class="btn btn-danger font-weight-bold mb-2"
         @click="delToMyFavorites"
       >
-        - favori
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>
       </button>
     </div>
     <div class="card-body m-0 p-0 overflow-auto">
@@ -97,7 +100,7 @@ export default {
       infos: [],
       isLoading: true,
       lastUpdate: "",
-      timer: 10000, // timer to refresh the live data
+      timer: 10000, // timer to refresh the live data 10s
       setTimer: "", // save the timer interval
     };
   },
