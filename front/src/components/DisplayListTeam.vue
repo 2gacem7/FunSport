cc0fe3b1fd27577d9ae778ed0eff505fae263f27
Your Name <you@example.com>
2020-10-13 16:46:34 +0200

    pagination

--- front/src/components/DisplayListTeam.vue
+++ front/src/components/DisplayListTeam.vue
@@ -6,9 +6,12 @@
       </button>
       <h3 class="text-dark text-center">{{ sport }} Team</h3>
       <button v-if="delButton" class="btn btn-danger font-weight-bold mb-2" @click="delToMyFavorites">
-        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+          <path fill-rule="evenodd"
+            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
         </svg>
       </button>
     </div>
@@ -24,34 +27,38 @@
       <table class="table">
         <tbody class="card m-0 p-0 overflow-auto" style="max-height: 20rem">
           <tr v-for="item in info" :key="item.id" class="w-100">
-            <td>
-              <button class="btn btn-success btn-sm rounded-circle mb-2"
-                      @click="addTeamToMyFavorite(item)">Add</button>
+            <td v-for ="item in favori" :key="item.id">
+              <button class="btn btn-success btn-sm rounded-circle mb-2" @click="addTeamToMyFavorite(item)">Add</button>
             </td>
             <td style="width: 100%">
               {{item.name}}
             </td>
             <td>{{item.location}}</td>
           </tr>
+          <tr v-if="info.length==0"> No more teams </tr>
         </tbody>
+        <tfoot>
+          <p>
+            <button v-if="page>1" @click="prev()">prev</button>
+            <button v-if="info.length>0" @click="next()">next</button>
+          </p>
+        </tfoot>
       </table>
     </div>
   </div>
-  
-
-
-
 
 </template>
 
 <script>
   import ENV from "../../env.config";
+
   export default {
     name: "DisplayListTeam",
 
     data() {
       return {
         info: {},
+        page: 1,
       };
     },
     props: {
@@ -74,9 +81,20 @@
             type: "team",
             name: teamSlug
           },
-        });
 
+        });
       },
+      async next() {
+        this.page++;
+        this.getInfos();
+      },
+      async prev() {
+        if (this.page > 1) {
+          this.page--;
+          this.getInfos();
+        }
+      },
+
       delToMyFavorites() {
         this.$emit("delfavorite", this.id);
       },
@@ -94,13 +112,13 @@
       async getInfos() {
         var myHeaders = new Headers();
         myHeaders.append("Authorization", "Bearer " + ENV.API_PANDA_SPORT);
-
         var requestOptions = {
           method: 'GET',
           headers: myHeaders,
           redirect: 'follow'
         };
-        await fetch(`https://api.pandascore.co/${this.apiName}/teams?sort=name&per_page=100`, requestOptions)
+        await fetch(`https://api.pandascore.co/${this.apiName}/teams?sort=name&per_page=10&page[number]=${this.page}`,
+            requestOptions)
           .then(response => response.json())
           .then(result => this.info = result)
           .catch(error => console.log('error', error));
@@ -110,10 +128,10 @@
 </script>
 
 <style scoped>
-tbody {
-  color: black;
-  font-family: Arial, Helvetica, sans-serif;
-}
+  tbody {
+    color: black;
+    font-family: Arial, Helvetica, sans-serif;
+  }
 
   thead {
     font-family: counter-strike;
