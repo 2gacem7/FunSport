b574dd0a1fa4db51aa3b742ee632b4a854d6e175
Kevin-Bridonneau <kevin.bridonneau@epitech.eu>
2020-10-13 16:41:57 +0200

    json package

--- env.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    API_SPORT_RADAR_CSGO : "m2xmrw2gf62v42anfjngmjqz",
-    API_SPORT_RADAR_DOTA : "",
-    API_SPORT_RADAR_BADMINTON:"bf23tvesuz2mcfwku9rzsqea",
-    API_SPORT_RADAR_LOL:""
-};
