67722630f1e926f950774361486ce18c23443011
Kevin-Bridonneau <kevin.bridonneau@epitech.eu>
2020-10-14 14:12:29 +0200

    cleaning old component

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 import Register from "../views/Register.vue";
 import Login from "../views/Login.vue";
 import MyProfile from "../views/MyProfile.vue";
