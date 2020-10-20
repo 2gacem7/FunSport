e635c6f75ba0feb2858a98235552d51c60da98ae
Yobar73 <bthl.yohann73@gmail.com>
2020-10-20 15:13:23 +0200

    B: Add documentation

--- back/src/dto/create-user.dto.ts
+++ back/src/dto/create-user.dto.ts
@@ -1,24 +1,47 @@
 import { IsEmail, Matches , MinLength, IsOptional} from 'class-validator';
 
+/**
+ * Regex used to check if password is validated.
+ */
 const passwordRegex = new RegExp('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})');
 
+/**
+ * This Dto is used to create a users.
+ */
 export class CreateUserDto {
-
+ /**
+   * firstname of the user
+   */
   @MinLength(1)
   firstName: string;
 
+  /**
+   * Lastname of the user
+   */
   @MinLength(1)
   lastName: string;
 
+  /**
+   * Email of the user
+   */
   @IsEmail()
   email: string;
 
+  /**
+   * Phone of the user (min 10)
+   */
   @MinLength(10)
   phone: string;
 
+  /**
+   * Password of the user using regex
+   */
   @Matches(passwordRegex)
   password: string;
 
+  /**
+   * Status administrator for this user (optionnal)
+   */
   @IsOptional()
   isAdmin: boolean;
 
--- back/src/dto/update-user.dto.ts
+++ back/src/dto/update-user.dto.ts
@@ -1,21 +1,41 @@
 import { IsEmail, Matches , MinLength, ValidateIf} from 'class-validator';
 
+/**
+ * Regex used to check if password is validated.
+ */
 const passwordRegex = new RegExp('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})');
 
+/**
+ * This Dto is used to update a users.
+ */
 export class UpdateUserDto {
-
+/**
+   * firstname of the user
+   */
   @MinLength(1)
   firstName: string;
 
+  /**
+   * Lastname of the user
+   */
   @MinLength(1)
   lastName: string;
 
+  /**
+   * Email of the user
+   */
   @IsEmail()
   email: string;
 
+  /**
+   * Phone of the user (min 10)
+   */
   @MinLength(10)
   phone: string;
 
+  /**
+   * Password of the user using regex only if password !=''
+   */
   @Matches(passwordRegex)
   @ValidateIf( p => p.password !== "")
   password: string;
